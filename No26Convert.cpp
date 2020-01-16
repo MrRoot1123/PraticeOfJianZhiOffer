e02db2482f6706f0e7a5edab16525a9e38e5ac20
MrRoot1123 <lucky_gen@foxmail.com>
2020-01-16 13:37:50 +0800

    No26Covert with queue

--- No26Convert.cpp
+++ No26Convert.cpp
@@ -2,7 +2,7 @@
 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。要求不能创建任何新的结点，只能调整树中结点指针的指向。
 */
 #include <iostream>
-
+#include <queue>
 using namespace std;
 
 struct TreeNode {
@@ -17,21 +17,35 @@ class Solution {
 public:
     TreeNode* Convert(TreeNode* pRootOfTree)
     {
-        
+        if (pRootOfTree == nullptr) return nullptr;
+        dfs(pRootOfTree);
+        TreeNode* pNew = queueNode.front();
+        TreeNode* pNode = pNew;
+        queueNode.pop();
+        while (!queueNode.empty())
+        {
+            TreeNode* pTmp;
+            pTmp = queueNode.front();
+            pNode -> right = pTmp;
+            pTmp -> left = pNode;
+            pNode = pTmp;
+            queueNode.pop();
+        }
+        return pNew;
     }
 private:
     void dfs(TreeNode* pRootOfTree)
     {
+        if (pRootOfTree == nullptr) return;
         if (pRootOfTree -> left != nullptr)
         {
             dfs(pRootOfTree -> left);
         }
-        else
+        queueNode.push(pRootOfTree);
+        if (pRootOfTree -> right != nullptr)
         {
-            
+            dfs(pRootOfTree -> right);
         }
-        
     }
-    TreeNode* pHead;
-    TreeNode* pNode;
+    queue<TreeNode*> queueNode;
 };
